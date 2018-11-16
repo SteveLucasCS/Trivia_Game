d33b50404b504192a099f6f212616c9a8d5688a1
stevelucascs <stevelucascs@gmail.com>
2018-11-16 13:33:21 -0600

    first upload

--- assets/javascript/gamelogic.js
+++ assets/javascript/gamelogic.js
@@ -1,29 +1,30 @@
 /********************************************************************************
  * @author Steve Lucas (stevelucas.github.io)
  * This is a simple trivia game, where the user selectes from multiple choice
- * answers. Questions can be created manually or generated from a db/API call.
+ * answer. Questions can be created manually or generated from a db/API call.
  * The user's score keeps track of how many questions they have gotten correct,
  * and the game ends when they answer the final question (the last question
  * object)
  ********************************************************************************/
 
- TODO - iterate through questions (starting at questionArray[0])
-      - build checkAnswer function
-      - Keep going from there
-/** Keeps track of user's score (correct answers) */
+/** Keeps track of user's score (correct answer) */
 var score = 0
 /** Current round of the game (used to iterate through questions).
  *  set to -1 by default to indicate it's the tutorial screen */
 var round = -1
 /** Array of HTML elements containing choices' text */
-var choicesText = $(".choice-text")
+var choicesText = $(".choice-text").toArray()
 
 /** Array of checkboxes corresponding to choices */
-var choicesCheck = $(".form-check-input")
+var choicesCheck = $("input:radio").toArray()
 
-var srcDifficulty = "medium"
-var srcComputerScience = "https://opentdb.com/api.php?amount=10&category=18&difficulty=" + srcDifficulty + "&type=multiple"
-var srcVideoGames = "https://opentdb.com/api.php?amount=10&category=15&difficulty=" + srcDifficulty + "&type=multiple"
+/** The indeces(or index) of correct answer(s) */
+var answerIndex = 0
+
+var srcDifficulty = "easy"
+var srcComputerScience = `https://opentdb.com/api.php?amount=10&category=18&difficulty=${srcDifficulty}&type=multiple`
+var srcVideoGames = `https://opentdb.com/api.php?amount=10&category=15&difficulty=${srcDifficulty}&type=multiple`
+var srcMythology = `https://opentdb.com/api.php?amount=10&category=20&difficulty=${srcDifficulty}&type=multiple`
 /** amount (integer) of questions to pull (# of rounds/questions in game) */
 var srcAmount = 10
 
@@ -32,21 +33,39 @@ var srcAmount = 10
  * Question class contains the properties
  * @property text - Displayed to the user via HTML
  * @property {string[]} choices - array of multiple choice options for the question
- * @property {string[]} answers - array of true/false answers to corresponding choices^
+ * @property {string[]} answer - array of true/false answer to corresponding choices^
  */
 class Question {
   /** Constructor for a question object
    * @param {string} question - string that will be displayed as the question in UI
    * @param {string[]} choices - array of choices given to user
-   * @param {string[]} answers - array of answers corresponding to the choice at the same index
+   * @param {string[] || string} answer - array of answer corresponding to the choice at the same index
    */
-  constructor(question, choices, answers) {
+  constructor(question, choices, answer) {
+    
     this.question = question
     this.choices = choices
-    this.answers = answers
+    this.answer = answer
+    this.choices.push(answer)
+    console.log(this.question)
+
   }
 }
 
+filterString("in &quot;quotes&quot;")
+filterString("Pok&eacute;mon")
+function filterString(input) {
+
+  var regex = /&(.*?);/g
+  var math = input.match(regex, "*")
+  
+  console.log("filted: " + output)
+}
+
+/**
+ * Builds a questionArray by pulling questions from a databse API
+ * @param {string} source - url to send get request
+ */
 function downloadQuestions(source) {
   //results will be set to an array of object returned from API call
   var results
@@ -54,6 +73,7 @@ function downloadQuestions(source) {
     console.log("API request Successful")
     results = response.results
     buildQuestionList(results)
+    $("#next-btn").prop("disabled", false)
   }
   var failed = function() {
     console.log("API quest Failed. Switching to local question defaults")
@@ -63,6 +83,7 @@ function downloadQuestions(source) {
     method: "GET"
   }).then(successful, failed)
 }
+
 
 function buildQuestionList(results) {
   /** Each object from opentdb trivia database contains the following properties
@@ -75,9 +96,9 @@ function buildQuestionList(results) {
    */
 
   for (var i = 0; i < results.length; i++) {
-    questionArray.push(new Question(results[i].question),
-      results[i].incorrect_answers, results[i].correct_answer)
-  }
+    questionArray.push(new Question(results[i].question,
+      results[i].incorrect_answers, results[i].correct_answer))
+    }
 }
 
 
@@ -91,37 +112,58 @@ function defaultQuestions() {
 
 
 /**
- * Starts a new round in the game
- * Iterates through questions and rounds
+ * Checks if the checkbox corresponding to the correct answer is checked
+ * @return true if answer is correct
+ * @return false if answer is incorrect
  */
-function nextRound() {
-  round++
-  //TODO: iterate to next question
-  updateUI(CurQuestion.text, curQuestion.choices)
-}
-
-
-/**
- * TODO 
- */
-function checkAnswers(choices, answers) {
-  console.log("TODO: checkAnswers()");
+function checkAnswer() {
+  for (var i = 0; i < 4; i++) {
+    i === answerIndex ? $(choicesText[i]).css("color", "green") :
+      $(choicesText[i]).css("color", "red")
+  }
+  
+  if (choicesCheck[answerIndex].checked)
+    score++
+  
+  $("#score").text(score)
+  $("#next-btn").show()
 }
 
 /**
  * Update's HTML based on parameters
- * @param {string} text 
- * @param {string[]} choices 
- * @param {number} score 
+ * @param {Question} q- a question object representing the current question
  */
-function updateUI(text, choices, score) {
-  $("#question").text(text)
+function updateUI (q) {
+  // random number decides where in the choiceText array the correct answer will be
+  console.log(q.answer)
+  answerIndex =  Math.floor(Math.random() * (4 - 0) );
+  $("#question").text(q.question)
   $("#score").text(score)
-  for (var i = 0; i < choices.length; i++) {
-    choicesText[i] = choices[i]
+  //current index of choices
+  var c = 0
+  //current index of choicesText
+  var i = 0
+  while ( i < 4) {
+    // i increments every loop
+    // c only increments if an incorrect answer is written
+    if (i === answerIndex) {
+      $(choicesText[i]).text(q.answer)
+    } else {
+      $(choicesText[i]).text(q.choices[c])
+      c++
+    }
+
+    //unchecks checked box
     if (choicesCheck[i].checked)
       choicesCheck[i].checked = false
+    
+    $(choicesText[i]).css("color", "black")
+    $("#round").text(round+1)
+    //total number of rounds
+    $("#total-rounds").text(srcAmount)
+    i++
   }
+  $("#next-btn").prop("disabled", true)
 }
 
 /** Current question object based on the round */
@@ -134,24 +176,29 @@ var defaultQuestionArray = defaultQuestions()
 var questionArray = []
 
 // Main execution
-downloadQuestions(srcComputerScience)
+downloadQuestions(srcVideoGames)
 
+$(choicesCheck).on("click", function () {
+  $("#submit-btn").prop("disabled", false)
+})
 
 // called when user clicks submit-btn
-$("#submit-btn").on("click", function() {
-  updateUI(curQuestion.text, curQuestion.choices, score)
-  //tutorial screen, does not check answers
-  if (round > -1) {
+$("input:radio").on("click", function() {
+  
+  if (round >= 0)
+    checkAnswer()
+  $("#next-btn").prop("disabled", false)
+  
+})
+
+$("#next-btn").on("click", function() {
+  if (round < questionArray.length-1) {
     round++
-    return
-  }
-  //checks if ALL answers were correct
-  if (checkAnswers(curQuestion.choices, curQuestion.answers)) {
-    score++
-    nextRound()
-  } else {
-    nextRound()
   }
 
-
+  else {
+    alert("Game Over!")
+    round = 0
+  }
+  updateUI(questionArray[round])
 })
